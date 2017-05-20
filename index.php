ac52ac209c440fccc33a02167011d9fb47647d55
jeeve <jvjulien@free.fr>
2017-05-20 22:25:14 +0200

    video

--- index.php
+++ index.php
@@ -2,4 +2,18 @@
 
 echo 'makevideo';
 
+
+
+$mov = new ffmpeg_movie('/video/video.avi');
+
+echo sprintf('<p>Duration : %.1f, bit rate : %d, frame count : %d, video codec : %s, audio codec : %s, audio channel : %s</p>',
+                $mov->getDuration(),
+                $mov->getBitRate(),
+                $mov->getFrameCount(),
+                $mov->getVideoCodec(),
+                $mov->getAudioCodec(),
+                $mov->getAudioChannels());
+
+
+
 ?>
