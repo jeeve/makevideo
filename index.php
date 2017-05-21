4ae9b588e7b14d8b26d55a6cbf552bf0c1dd987b
jeeve <jvjulien@free.fr>
2017-05-21 14:56:23 +0200

    video

--- index.php
+++ index.php
@@ -11,8 +11,12 @@ echo $mov->getPathfile();
 
 echo 'makevideo';
 
+$tempfile = tempnam(sys_get_temp_dir(), 'timelapse.mp4');
+
 echo "Starting ffmpeg...\n\n";
-echo shell_exec("ffmpeg -f image2 -framerate 25 -i video/E%2d.jpg -r 25 -an -vcodec mpeg4 -b 15000k video/rendu_final.mp4");
+echo shell_exec("ffmpeg -f image2 -framerate 25 -i video/E%2d.jpg -r 25 -an -vcodec mpeg4 -b 15000k $tempfile");
 echo "Done.\n";
+
+echo '<video controls src="' . $tempfile . '"></video>';
 
 ?>
