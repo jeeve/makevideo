94dac14e7061049af00b1740c90e2307e6069198
Jeeve <jvjulien@free.fr>
2017-05-21 22:41:42 +0200

    exec

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $tempfile = 'tmp/timelapse.mp4';
 $shellline = "ffmpeg -f image2 -framerate 25 -i video/E%2d.jpg -r 25 -an -vcodec mpeg4 -b 15000k " . $tempfile;
 
 echo "Starting ffmpeg...\n\n";
-echo shell_exec($shellline);
+echo exec($shellline);
 echo "Done.\n";
 
 echo '<a href="' . $tempfile . '">fichier</a>'; 
