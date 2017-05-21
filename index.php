b80f1f77491c2d183019ab8fc4f1f43d3e49b949
jeeve <jvjulien@free.fr>
2017-05-21 15:20:19 +0200

    shell

--- index.php
+++ index.php
@@ -12,9 +12,10 @@ echo $mov->getPathfile();
 echo 'makevideo';
 
 $tempfile = sys_get_temp_dir() . '/timelapse.mp4';
+$shellline = "ffmpeg -f image2 -framerate 25 -i video/E%2d.jpg -r 25 -an -vcodec mpeg4 -b 15000k" . $tempfile;
 
 echo "Starting ffmpeg...\n\n";
-echo shell_exec("ffmpeg -f image2 -framerate 25 -i video/E%2d.jpg -r 25 -an -vcodec mpeg4 -b 15000k $tempfile");
+echo shell_exec($shellline);
 echo "Done.\n";
 
 echo '<video controls="controls">';
