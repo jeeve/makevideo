410e27af8acd7cb29c60a4a9c9fcbf7711ee21d6
jeeve <jvjulien@free.fr>
2017-05-21 12:12:11 +0200

    shell

--- index.php
+++ index.php
@@ -11,5 +11,8 @@ echo $mov->getPathfile();
 
 echo 'makevideo';
 
+echo "Starting ffmpeg...\n\n";
+echo shell_exec("ffmpeg -f image2 -framerate 25 -i video/E%2d.jpg -r 25 -an -vcodec mpeg4 -b 15000k video/rendu_final.mp4");
+echo "Done.\n";
 
 ?>
