3899d3c16deb0f1b7d86a2955c5ae8993254f0f2
jeeve <jvjulien@free.fr>
2017-05-22 10:24:07 +0200

    head

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
+
 header('AddType video/ogg .ogv');
 header('AddType video/ogg .ogg');
 header('AddType video/mp4 .mp4');
 header('AddType video/webm .webm');
-?>
-
-<?php
 
 $tempfile = 'tmp/timelapse.mp4';
 $shellline = "ffmpeg -f image2 -i video/E%2d.jpg -r 24 -vcodec mpeg4 -b 15000k " . $tempfile;
