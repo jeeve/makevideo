e94beb70a605f6485c73008c4dadfb74664b2b6f
jeeve <jvjulien@free.fr>
2017-05-22 10:13:14 +0200

    ffmmpeg

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $tempfile = 'tmp/timelapse.mp4';
-$shellline = "ffmpeg -f image2 -framerate 25 -i video/E%2d.jpg -r 25 -an -vcodec libx264 -b 15000k " . $tempfile;
+$shellline = "ffmpeg -f image2 -i video/E%2d.jpg -r 24 -vcodec mpeg4 -b 15000k " . $tempfile;
 
 echo "Starting ffmpeg...\n\n";
 echo exec($shellline);
