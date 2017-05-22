2abb11d106bd2d8a2c1f70c72fcdf2a756ddaf0b
jeeve <jvjulien@free.fr>
2017-05-22 11:15:09 +0200

    jpg

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 $imgSrc = "http://imagebankleryposes.appspot.com/dispimg?date=21-05-2017&time=10:00";
-copy($imgSrc, "tmp/P001");
+copy($imgSrc, "tmp/P001.jpg");
 
 $imgSrc = "http://imagebankleryposes.appspot.com/dispimg?date=21-05-2017&time=10:01";
-copy($imgSrc, "tmp/P002");
+copy($imgSrc, "tmp/P002.jpg");
 
 $tempfile = 'tmp/timelapse.mp4';
 $shellline = "ffmpeg -f image2 -i tmp/P%3d.jpg -r 24 -vcodec mpeg4 -b 15000k " . $tempfile;
