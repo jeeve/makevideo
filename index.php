da3dbaee79748b2d719d7748a9ae410b737f3bf6
jeeve <jvjulien@free.fr>
2017-05-22 09:55:22 +0200

    remove

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
 
-require('vendor/autoload.php');
-
-
-$ffmpeg = FFMpeg\FFMpeg::create();
-
-$mov = $ffmpeg->open('video/video.avi');
-
-echo $mov->getPathfile(); 
-
-echo 'makevideo';
-
 $tempfile = 'tmp/timelapse.mp4';
 $shellline = "ffmpeg -f image2 -framerate 25 -i video/E%2d.jpg -r 25 -an -vcodec mpeg4 -b 15000k " . $tempfile;
 
