b419284895baf3dfde5826fea8d2a9c10ec34f4f
jeeve <jvjulien@free.fr>
2017-05-22 21:16:54 +0200

    r10

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ while ($d < $d2) {
 
 $tempfile = 'tmp/timelapse.mp4';
 //$shellline = "ffmpeg -f image2 -i tmp/P%4d.jpg -r 5 -vcodec mpeg4 -b 15000k " . $tempfile;
-$shellline = "ffmpeg -f image2 -i tmp/P%04d.jpg -r 5 -vcodec libx264 -crf 25 " . $tempfile;
+$shellline = "ffmpeg -f image2 -i tmp/P%04d.jpg -r 10 -vcodec libx264 -crf 25 " . $tempfile;
 
 exec($shellline);
 /*
